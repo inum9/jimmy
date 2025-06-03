f419e3ca83c4455a585eb78f4957e2c7bb7b6cd2
inum9 <inum9@outlook>
2025-06-04 02:08:13 +0530

    implemnt frontend and ui component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import AppRoutes from './routes/AppRoutes'
+const App = () => {
+  return (
+    <div className='text-2xl border-4 border-red-500 bg bg-red-600 text-blue-500'>
+      app 
+    </div>
+  )
+}
+
+export default App
