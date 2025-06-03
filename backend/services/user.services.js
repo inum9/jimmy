1a75867f4ec6a5285aebe3411a3c815409acd43f
inum9 <inum9@outlook>
2025-06-03 16:19:35 +0530

    routing servicing

---
+++ backend/services/user.services.js
@@ -0,0 +1,26 @@
+import User from "../model/user.model.js";
+export const createUser = async ({
+    email, password
+}) => {
+
+    if (!email || !password) {
+        throw new Error('Email and password are required');
+    }
+
+    const hashedPassword = await User.hashPassword(password);
+
+    const user = await User.create({
+        email,
+        password: hashedPassword
+    });
+
+    return user;
+
+}
+
+export const getAllUsers = async ({ userId }) => {
+    const users = await User.find({
+        _id: { $ne: userId }
+    });
+    return users;
+}
