56abe88bfefcde48a16e0864f1feb693a72d6d45
inum9 <inum9@outlook>
2025-06-04 14:00:55 +0530

    implement the socket io

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,88 @@
+import "dotenv/config";
 import http from "http";
-import dotenv from "dotenv"
-dotenv.config({
-    path: './.env'
-});
 import app from "./app.js";
-const port= process.env.PORT||8000;
+import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import projectModel from "./model/project.model.js";
+// import { generateResult } from "./services/ai.service.js";
 
-const server= http.createServer(app);
+const port = process.env.PORT || 3000;
 
-server.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
-    
-})
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.use(async (socket, next) => {
+  try {
+    const token =
+      socket.handshake.auth?.token ||
+      socket.handshake.headers.authorization?.split(" ")[1];
+    const projectId = socket.handshake.query.projectId;
+
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+      return next(new Error("Invalid projectId"));
+    }
+
+    socket.project = await projectModel.findById(projectId);
+
+    if (!token) {
+      return next(new Error("Authentication error"));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return next(new Error("Authentication error"));
+    }
+
+    socket.user = decoded;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.roomId = socket.project._id.toString();
+
+  console.log("a user connected");
+
+  socket.join(socket.roomId);
+
+  socket.on("project-message", async (data) => {
+    const message = data.message;
+
+    const aiIsPresentInMessage = message.includes("@ai");
+    socket.broadcast.to(socket.roomId).emit("project-message", data);
+
+    if (aiIsPresentInMessage) {
+      const prompt = message.replace("@ai", "");
+
+      const result = await generateResult(prompt);
+
+      io.to(socket.roomId).emit("project-message", {
+        message: result,
+        sender: {
+          _id: "ai",
+          email: "AI",
+        },
+      });
+
+      return;
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    socket.leave(socket.roomId);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
