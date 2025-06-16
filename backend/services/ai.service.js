7513765e563ddd9fae3be3873e16c9595ff29b6e
inum9 <inum9@outlook>
2025-06-17 00:14:45 +0530

    debuuged ai

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
     generationConfig: {
